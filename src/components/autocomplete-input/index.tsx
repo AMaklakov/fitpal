89f2b15b262030f279a7ffd74dff0f78c6f53bfc
IsNotMyMaster <asterwarden94@gmail.com>
2020-04-12 19:17:04 +0300

    feat(autocomplete-input): add styles to scrollArea, add FlatList method in scrollView

--- src/components/autocomplete-input/index.tsx
+++ src/components/autocomplete-input/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { AutocompleteInputProps, SelectedItemViewComponentProps } from './types';
-import { ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { FlatList, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { autocomplete } from './helpers';
 import style from './style';
 import { placeholderTextColor } from '../../css/colors.style';
@@ -31,9 +31,7 @@ const AutocompleteInput = <T extends Object>(props: AutocompleteInputProps<T>) =
 	const onChangeTextHandler = (v: string): void => {
 		changeValue(v);
 
-		setFilteredAutocompleteList(
-			v ? autocomplete({ value: v, autocompleteList, autocompleteField }) : []
-		);
+		setFilteredAutocompleteList(v ? autocomplete({ value: v, autocompleteList, autocompleteField }) : []);
 	};
 
 	const selectAutocompletion = (autocompleteObj: T) => () => {
@@ -67,13 +65,17 @@ const AutocompleteInput = <T extends Object>(props: AutocompleteInputProps<T>) =
 
 			{filteredAutocompleteList?.length > 0 && (
 				<ScrollView style={style.scrollArea}>
-					{filteredAutocompleteList.map((x, i) => (
-						<TouchableOpacity key={`autocomplete-field-${i}`} onPress={selectAutocompletion(x)}>
-							<View>
-								<Text>{x[autocompleteField]}</Text>
-							</View>
-						</TouchableOpacity>
-					))}
+					<FlatList
+						data={filteredAutocompleteList}
+						keyExtractor={(s, index) => `series_${index}`}
+						renderItem={({ item }) => (
+							<TouchableOpacity key={`autocomplete-field-${item}`} onPress={selectAutocompletion(item)}>
+								<View>
+									<Text style={style.h3}>{item[autocompleteField]}</Text>
+								</View>
+							</TouchableOpacity>
+						)}
+					/>
 				</ScrollView>
 			)}
 		</View>
