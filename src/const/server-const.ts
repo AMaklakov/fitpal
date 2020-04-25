f17db1a102e249a9733d1990a20212f441921e83
Artyom Maklakov <mavgisa@yandex.ru>
2020-04-25 14:12:55 +0300

    chore(http): set up env for android emulator

--- src/const/server-const.ts
+++ src/const/server-const.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 /**
  * Should contain IP of a server, because android runs in an emulator
  * and have no access to 0.0.0.0
  */
 export const SERVER_URL = 'http://192.168.100.26:3001';
-=======
-export const SERVER_URL = 'http://0.0.0.0:3001';
->>>>>>> feature(http-requests): add covid saga
