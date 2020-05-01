bfb223feee2a8d45bee38e80eb342c2c5d5ab8ae
Artyom Maklakov <MavGisa@yandex.ru>
2020-05-01 23:57:45 +0300

    fix(rebase-problems): fix rebase problems

--- src/model/fetch-state.model.ts
+++ src/model/fetch-state.model.ts
@@ -1,8 +1,4 @@
 export interface IFetchState {
 	loading: boolean;
-<<<<<<< HEAD
 	error: string | object | null;
-=======
-	error: string | null;
->>>>>>> feature(http-requests): add covid saga
 }
--- src/util/axios.ts
+++ src/util/axios.ts
@@ -2,9 +2,5 @@ import axiosLib from 'axios';
 import { SERVER_URL } from '@const/server-const';
 
 export const axios = axiosLib.create({
-<<<<<<< HEAD
 	baseURL: `${SERVER_URL}/api/v1/`,
-=======
-	baseURL: `${SERVER_URL}/api`,
->>>>>>> feature(http-requests): add covid saga
 });
