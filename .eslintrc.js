412011890f0a5424b379d42e2dd9eb4c3028a873
Artyom Maklakov <MavGisa@yandex.ru>
2020-05-03 18:09:15 +0300

    feat(hooks): add `useAsyncStorage` hook and hooks eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
 	root: true,
 	extends: ['@react-native-community', 'prettier', 'prettier/react'],
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint', 'prettier'],
+	plugins: ['@typescript-eslint', 'react-hooks', 'prettier'],
 	rules: {
 		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
 		'no-console': 'error',
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'error',
 	},
 };
