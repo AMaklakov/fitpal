ae5cbad22607eb28e1440545135721f901ec3290
Artyom Maklakov <MavGisa@yandex.ru>
2020-05-03 18:26:14 +0300

    feat(server-url): change server url with prod one

--- src/const/server-const.ts
+++ src/const/server-const.ts
@@ -2,4 +2,4 @@
  * Should contain IP of a server, because android runs in an emulator
  * and have no access to 0.0.0.0
  */
-export const SERVER_URL = 'http://192.168.100.30:3001';
+export const SERVER_URL = 'https://fitpal.tk:443';
