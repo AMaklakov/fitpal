2982e698046486151274156bab50b6938ba61ac3
Artyom Maklakov <MavGisa@yandex.ru>
2020-05-05 00:18:55 +0300

    feat(actions): add simple actions creator to reduce boilerplate

--- src/model/data-action.model.ts
+++ src/model/data-action.model.ts
@@ -6,3 +6,13 @@ export interface DataAction<Payload = any> extends Action<string> {
 }
 
 export type DataActionCreator<T = any> = (data: T, ...args: any[]) => DataAction<T>;
+
+export const createFetchActions = <Payload1, Payload2, Payload3, Names extends string>([
+	name1,
+	name2,
+	name3,
+]: Names[]): [DataActionCreator<Payload1>, DataActionCreator<Payload2>, DataActionCreator<Payload3>] => [
+	data => ({ type: name1, payload: data }),
+	data => ({ type: name2, payload: data }),
+	data => ({ type: name3, payload: data }),
+];
