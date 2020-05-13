f64d7b78da3a1c6aa70f66073a740ee695bfac07
Artyom Maklakov <MavGisa@yandex.ru>
2020-05-13 21:38:13 +0300

    feat(icons): add chevron-left.icon

---
+++ src/components/icons/chevron-left.icon.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { BaseMaterialIcon } from '@icons/base-material-icon';
+import { IconProps } from '@icons/model/icon-props.type';
+
+export const ChevronLeftIcon = (props: IconProps) => <BaseMaterialIcon name="chevron-left" {...props} />;
