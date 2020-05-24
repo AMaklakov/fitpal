faca4e2b90bcd811a5a7999f04528c0609a1e304
asterwarden <59479978+asterwarden@users.noreply.github.com>
2020-05-24 21:12:14 +0300

    Redesign calendar-strip (#47)

--- src/components/calendar/calendar-strip.tsx
+++ src/components/calendar/calendar-strip.tsx
@@ -4,34 +4,58 @@ import Strip from 'react-native-calendar-strip';
 import { getToday } from '../../util/date.util';
 import { Colors } from '../../css/colors.style';
 import moment from 'moment';
+import { toRgba } from '@util/css.util';
+import { Fonts } from '@css/fonts';
 
 const styles = StyleSheet.create({
 	headingText: {
-		// backgroundColor: 'pink',
+		color: Colors.Primary,
+		paddingTop: 10,
+		paddingBottom: 0,
+		fontSize: 32,
+		fontFamily: Fonts.KelsonBold,
 	},
 	calendar: {
-		height: 100,
+		minHeight: 160,
 		paddingTop: 10,
 		paddingBottom: 10,
+		backgroundColor: Colors.Lightgray,
+		color: Colors.White,
+		fontFamily: Fonts.KelsonBold,
+	},
+	dateNumberStyle: {
+		color: Colors.Primary,
+		fontSize: 16,
 	},
 });
 
 interface CalendarStripProps {
 	selectedDate: moment.Moment;
-	changeSelectedDate: (selectedDate: moment.Moment) => void;
+	changeSelectedDate: (selectedDate: moment.MomentInput) => void;
 }
 
 export const CalendarStrip = (props: CalendarStripProps) => {
 	const { selectedDate = getToday(), changeSelectedDate } = props;
 
-	const daySelectionAnimation = useMemo(
+	const customDatesStyles = useMemo(
+		() => [
+			{
+				startDate: moment(),
+				dateNameStyle: { color: Colors.White },
+				dateNumberStyle: { color: Colors.White },
+				dateContainerStyle: { backgroundColor: toRgba(Colors.Red, 0.5) },
+			},
+		],
+		[]
+	);
+
+	const daySelectionAnimation = useMemo<any>(
 		() =>
 			({
-				type: 'border',
+				type: 'background',
 				duration: 200,
-				borderWidth: 1,
-				borderHighlightColor: Colors.LightBlue,
-			} as any),
+				highlightColor: Colors.Purple,
+			}),
 		[]
 	);
 
@@ -39,10 +63,17 @@ export const CalendarStrip = (props: CalendarStripProps) => {
 		<View>
 			<Strip
 				style={styles.calendar}
-				selectedDate={(selectedDate as unknown) as Date}
-				onDateSelected={changeSelectedDate as any}
-				calendarAnimation={{ type: 'sequence', duration: 50 }}
+				calendarHeaderStyle={styles.headingText}
+				dateNumberStyle={styles.dateNumberStyle}
+				dateNameStyle={{ color: Colors.Primary }}
+				highlightDateNumberStyle={{ color: Colors.White }}
+				highlightDateNameStyle={{ color: Colors.White }}
+				weekendDateNameStyle={{ color: Colors.White }}
+				selectedDate={selectedDate.toDate()}
+				onDateSelected={changeSelectedDate}
+				calendarAnimation={{ type: 'sequence', duration: 30 }}
 				daySelectionAnimation={daySelectionAnimation}
+				customDatesStyles={customDatesStyles}
 			/>
 		</View>
 	);
