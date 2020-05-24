b32bd0bdc068baa821b4176a4a015069dcea451f
Artyom Maklakov <MavGisa@yandex.ru>
2020-05-24 21:17:54 +0300

    feat(training-minimal-view): start work on training-list redesign

--- src/components/training-minimal-view/training-list-minimal-view.tsx
+++ src/components/training-minimal-view/training-list-minimal-view.tsx
@@ -1,11 +1,7 @@
-import { TrainingModel } from '../../model/training.model';
-import { StyleSheet, View } from 'react-native';
+import { TrainingModel } from '@model/training.model';
+import { FlatList } from 'react-native';
 import React from 'react';
 import { TrainingMinimalView } from './training-minimal-view';
-import { SwipeListView } from 'react-native-swipe-list-view';
-import { Colors } from '../../css/colors.style';
-import { SwipeHiddenButton } from '../swipe-list/button';
-import { useTranslation } from 'react-i18next';
 
 interface TrainingListMinimalViewProps {
 	trainingList?: TrainingModel[];
@@ -15,54 +11,20 @@ interface TrainingListMinimalViewProps {
 	onDelete: (training: TrainingModel) => void;
 }
 
-const styles = StyleSheet.create({
-	rowBack: {
-		flex: 1,
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-	},
-	button1: {
-		flex: 1,
-		alignItems: 'flex-start',
-	},
-	button2: {
-		flex: 1,
-		alignItems: 'flex-end',
-	},
-});
-
 export const TrainingListMinimalView = (props: TrainingListMinimalViewProps) => {
 	const { trainingList, onTrainingPress, onCopy = () => {}, onDelete = () => {} } = props;
-	const { t } = useTranslation();
 
 	return (
-		<SwipeListView<TrainingModel>
+		<FlatList<TrainingModel>
 			data={trainingList}
-			renderItem={data => <TrainingMinimalView training={data.item} onTrainingPress={onTrainingPress} />}
-			renderHiddenItem={data => (
-				<View style={styles.rowBack}>
-					<SwipeHiddenButton
-						style={styles.button1}
-						title={t('Copy')}
-						item={data.item}
-						textColor={Colors.White}
-						onTouch={onCopy}
-					/>
-
-					<SwipeHiddenButton
-						style={styles.button2}
-						title={t('Delete')}
-						item={data.item}
-						onTouch={onDelete}
-						backgroundColor={Colors.LightRed}
-						textColor={Colors.White}
-					/>
-				</View>
+			renderItem={data => (
+				<TrainingMinimalView
+					training={data.item}
+					onTrainingPress={onTrainingPress}
+					onDelete={onDelete}
+					onCopy={onCopy}
+				/>
 			)}
-			leftOpenValue={115}
-			stopLeftSwipe={130}
-			rightOpenValue={-115}
-			stopRightSwipe={-130}
 		/>
 	);
 };
--- src/components/training-minimal-view/training-minimal-view.tsx
+++ src/components/training-minimal-view/training-minimal-view.tsx
@@ -1,48 +1,99 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import React, { useCallback } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { TrainingModel } from '@model/training.model';
 import { H2 } from '@components/heading/h2';
+import { Card, Divider, Icon, ListItem, Tooltip } from 'react-native-elements';
+import { useTranslation } from 'react-i18next';
+import { Fonts } from '@css/fonts';
 import { Colors } from '@css/colors.style';
-
-const styles = StyleSheet.create({
-	container: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		paddingHorizontal: 20,
-		paddingVertical: 10,
-		backgroundColor: Colors.LightBlue,
-	},
-	description: {
-		maxWidth: 70 + '%',
-	},
-	exerciseWrap: {
-		maxWidth: 30 + '%',
-	},
-});
 
 interface TrainingMinimalViewProps {
 	training: TrainingModel;
 	onTrainingPress?: (training: TrainingModel) => void;
+	onCopy?: (training: TrainingModel) => void;
+	onDelete?: (training: TrainingModel) => void;
 }
 
 export const TrainingMinimalView = (props: TrainingMinimalViewProps) => {
-	const { training, onTrainingPress } = props;
+	const { training, onTrainingPress, onCopy, onDelete } = props;
+	const { t } = useTranslation();
 
-	const handleOnPress = () => {
-		onTrainingPress?.(training);
-	};
+	const handleOnPress = useCallback(() => onTrainingPress?.(training), [onTrainingPress, training]);
+	const handleOnCopy = useCallback(() => onCopy?.(training), [onCopy, training]);
+	const handleOnDelete = useCallback(() => onDelete?.(training), [onDelete, training]);
 
 	return (
-		<TouchableHighlight onPress={handleOnPress}>
-			<View style={styles.container}>
-				<View style={styles.description}>
-					<H2 numberOfLinesEllipsis={1} text={training?.name} />
+		<Card
+			title={
+				<View style={styles.titleContainer}>
+					<TouchableOpacity onPress={handleOnPress}>
+						<H2 text={training?.name} />
+					</TouchableOpacity>
+
+					<Tooltip
+						withPointer={false}
+						containerStyle={styles.tooltip}
+						width={200}
+						popover={
+							<View style={styles.tooltipInner}>
+								<ListItem
+									title={t('Details')}
+									onPress={handleOnPress}
+									leftIcon={{ type: 'material', name: 'search' }}
+									titleStyle={styles.listItemTitle}
+								/>
+								<ListItem
+									title={t('Copy')}
+									onPress={handleOnCopy}
+									leftIcon={{ type: 'material', name: 'content-copy' }}
+									titleStyle={styles.listItemTitle}
+								/>
+								<ListItem
+									title={t('Delete')}
+									leftIcon={{ type: 'material', name: 'delete' }}
+									onPress={handleOnDelete}
+									titleStyle={styles.listItemTitle}
+								/>
+							</View>
+						}>
+						<Icon type="material" name="more-vert" />
+					</Tooltip>
 				</View>
-				<View style={styles.exerciseWrap}>
-					<Text>{training?.exerciseList?.length} упражнений</Text>
-				</View>
+			}>
+			<Divider />
+			<View style={styles.exerciseWrap}>
+				<Text>{training?.exerciseList?.length} упражнений</Text>
 			</View>
-		</TouchableHighlight>
+		</Card>
 	);
 };
+
+const styles = StyleSheet.create({
+	exerciseWrap: {
+		maxWidth: 30 + '%',
+	},
+	titleContainer: {
+		paddingBottom: 10,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	tooltip: {
+		height: 'auto',
+		backgroundColor: Colors.White,
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 1,
+		},
+		shadowOpacity: 0.2,
+		shadowRadius: 1.41,
+
+		elevation: 2,
+	},
+	tooltipInner: { width: '100%' },
+	listItemTitle: {
+		fontSize: 14,
+		fontFamily: Fonts.Kelson,
+	},
+});
