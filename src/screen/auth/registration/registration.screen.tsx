463204dfdf14e50d3f9a49553952191910cd19ac
Artyom Maklakov <MavGisa@yandex.ru>
2020-06-06 22:52:42 +0300

    styles(auth-zone): improve styling

--- src/screen/auth/registration/registration.screen.tsx
+++ src/screen/auth/registration/registration.screen.tsx
@@ -3,7 +3,6 @@ import { Alert, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { StringInput } from '@inputs/string-input/string-input';
 import { useTranslation } from 'react-i18next';
-import { Colors } from '@css/colors.style';
 import { IntegerNumberInput } from '@inputs/integer-number-input/integer-number-input';
 import { NavigationPropsModel } from '@model/navigation-props.model';
 import { Routes } from '@screen/navigator';
@@ -11,7 +10,7 @@ import { IRegisterRequestBody } from '@model/register-request-body.model';
 import { Dispatch } from 'redux';
 import { registerStart } from '@redux/action/user.action';
 import { Button } from '@components/button/button';
-import { FontSizes } from '@css/fonts';
+import { H1 } from '@components/heading/h1';
 
 interface IState {}
 
@@ -59,35 +58,22 @@ const Registration: FC<IProps & IState & IDispatch> = props => {
 	return (
 		<ScrollView>
 			<View style={styles.wrapper}>
-				<View style={styles.inputWrapper}>
-					<StringInput value={email} onChange={setEmail} label={t('Enter email')} />
-				</View>
+				<H1 text={t('Registration')} />
 
-				<View style={styles.inputWrapper}>
-					<StringInput value={password} onChange={setPassword} isPassword={true} label={t('Enter password')} />
-				</View>
-
-				<View style={styles.inputWrapper}>
-					<StringInput value={firstName} onChange={setFirstName} label={t('Enter first name')} />
-				</View>
-
-				<View style={styles.inputWrapper}>
-					<StringInput value={lastName} onChange={setLastName} label={t('Enter last name')} />
-				</View>
-
-				<View style={styles.inputWrapper}>
-					<StringInput value={middleName} onChange={setMiddleName} label={t('Enter middle name')} />
-				</View>
-
+				<StringInput value={email} onChange={setEmail} label={t('Enter email')} />
+				<StringInput value={password} onChange={setPassword} isPassword={true} label={t('Enter password')} />
+				<StringInput value={firstName} onChange={setFirstName} label={t('Enter first name')} />
+				<StringInput value={lastName} onChange={setLastName} label={t('Enter last name')} />
+				<StringInput value={middleName} onChange={setMiddleName} label={t('Enter middle name')} />
 				{/*	TODO add sex switch */}
-
-				<View style={styles.inputWrapper}>
-					<IntegerNumberInput label={t('Enter age')} value={age} onChange={setAge} />
-				</View>
-
-				<View style={styles.inputWrapper}>
-					<IntegerNumberInput label={t('Enter weight')} value={weight} onChange={setWeight} />
-				</View>
+				<IntegerNumberInput label={t('Enter age')} value={age} onChange={setAge} />
+				<IntegerNumberInput
+					label={t('Enter weight')}
+					value={weight}
+					onChange={setWeight}
+					rightIcon={<Text>{t('Kg')}</Text>}
+					rightIconContainerStyle={styles.iconContainerStyle}
+				/>
 
 				<View style={styles.buttonsWrapper}>
 					<View>
@@ -105,28 +91,14 @@ const Registration: FC<IProps & IState & IDispatch> = props => {
 const styles = StyleSheet.create({
 	wrapper: {
 		alignItems: 'center',
-		paddingVertical: 20,
+		padding: 20,
 	},
-	inputWrapper: {
-		width: '85%',
-	},
+	iconContainerStyle: { marginVertical: 0 },
 	buttonsWrapper: {
-		width: '80%',
 		marginTop: 20,
 		flexDirection: 'row',
 		justifyContent: 'space-around',
 		alignItems: 'center',
-	},
-	registerWrapper: {
-		backgroundColor: Colors.LightBlue,
-	},
-	registerText: {
-		color: Colors.White,
-	},
-	link: {
-		fontSize: FontSizes.Medium,
-		color: Colors.DarkBlue,
-		textDecorationLine: 'underline',
 	},
 	loginButton: { paddingVertical: 0 },
 	loginContainer: { alignItems: 'baseline' },
