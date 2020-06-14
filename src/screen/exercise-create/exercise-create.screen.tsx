556baef52257e7c7614fc2f798d7f4bf5cd0034d
Artyom Maklakov <MavGisa@yandex.ru>
2020-06-14 18:25:37 +0300

    feat(exercise-description): add `description` field
    Add labels. Replace `paragraph` with `hint` font.

--- src/screen/exercise-create/exercise-create.screen.tsx
+++ src/screen/exercise-create/exercise-create.screen.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useCallback, useMemo, useState } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { connect, MapDispatchToPropsParam, MapStateToPropsParam } from 'react-redux';
 import { StoreModel } from '@redux/store';
 import { NavigationPropsModel } from '@model/navigation-props.model';
@@ -9,9 +9,13 @@ import { H1 } from '@components/heading/h1';
 import { StringInputWithValidation } from '@components/inputs/string-input/string-input';
 import { createExerciseStart, EXERCISE_ACTION_CREATORS } from '@redux/action/exercise.action';
 import { useTranslation } from 'react-i18next';
-import { EXERCISE_NAME_MAXLENGTH, EXERCISE_NAME_MINLENGTH } from '@const/validation-const';
-import { IErrors } from '@components/with-validation/with-validation';
+import { EXERCISE_VALID } from '@const/validation-const';
 import { Button } from '@components/button/button';
+import { SelectInput } from '@components/select-input';
+import { Text } from 'react-native-elements';
+import { Fonts, FontSizes } from '@css/fonts';
+import { Colors } from '@css/colors.style';
+import { validateExercise } from '@util/exercise.util';
 
 interface IProps extends NavigationPropsModel {}
 
@@ -28,40 +32,83 @@ const ExerciseCreate = (props: IProps & IStateToProps & IDispatchToProps) => {
 	const { exercise, onCreateExercise, onUpdateExercise, navigation } = props;
 	const { t } = useTranslation();
 
-	const [name, changeName] = useState(exercise?.name ?? '');
-	const [type] = useState<ExerciseTypes>(ExerciseTypes.Default);
-	const [isSaveDisabled, changeIsSaveDisabled] = useState(!exercise?.name);
+	const [name, setName] = useState(exercise?.name ?? '');
+	const [description, setDescription] = useState(exercise?.description ?? '');
+	const [type, setType] = useState<ExerciseTypes>(ExerciseTypes.Default);
+	const isSaveDisabled = useMemo(() => !validateExercise({ name, description, type }), [description, name, type]);
+
+	const EXERCISE_TYPES = useMemo(
+		() => [
+			{ label: t('Default'), value: ExerciseTypes.Default },
+			{ label: t('With additional weight'), value: ExerciseTypes.WithAdditionalWeight },
+			{ label: t('With negative weight'), value: ExerciseTypes.WithNegativeWeight },
+		],
+		[t]
+	);
+
+	const handleSetName = useCallback((value: string) => setName(value), []);
+	const handleSetDescription = useCallback((value: string) => setDescription(value), []);
+	const handleSetType = useCallback((value: ExerciseTypes | null) => setType(value ?? 0), []);
 
 	const handleGoBack = useCallback(() => navigation.goBack(), [navigation]);
-
 	const handleSavePress = useCallback(() => {
-		if (!name) {
+		if (isSaveDisabled) {
 			return;
 		}
 
-		exercise === undefined ? onCreateExercise({ name, type }) : onUpdateExercise({ ...exercise, name });
+		exercise === undefined
+			? onCreateExercise({ name, type, description })
+			: onUpdateExercise({ ...exercise, name, description });
 
 		handleGoBack();
-	}, [exercise, handleGoBack, name, onCreateExercise, onUpdateExercise, type]);
-
-	const handleChangeName = useCallback((name: string, errors?: IErrors) => {
-		changeName(name);
-		changeIsSaveDisabled(!!errors);
-	}, []);
+	}, [exercise, handleGoBack, name, onCreateExercise, onUpdateExercise, type, description, isSaveDisabled]);
 
 	return (
 		<View style={styles.wrapper}>
-			<H1 text={exercise ? t('Edit exercise') : t('Create exercise')} wrapperStyle={styles.h1} />
+			<ScrollView>
+				<View>
+					<H1 text={exercise ? t('Edit exercise') : t('Create exercise')} wrapperStyle={styles.h1} />
 
-			<View style={styles.mainContent}>
-				<StringInputWithValidation
-					value={name}
-					onChange={handleChangeName}
-					maxLength={[EXERCISE_NAME_MAXLENGTH, t('Max length is |len|', { len: EXERCISE_NAME_MAXLENGTH })]}
-					minLength={[EXERCISE_NAME_MINLENGTH, t('Min length is |len|', { len: EXERCISE_NAME_MINLENGTH })]}
-					placeholder={t('Exercise name')}
-				/>
-			</View>
+					<View style={styles.mainContent}>
+						<StringInputWithValidation
+							defaultValue={name}
+							onChange={handleSetName}
+							maxLength={[
+								EXERCISE_VALID.NAME.maxLength,
+								t('Max length is |len|', { len: EXERCISE_VALID.NAME.maxLength }),
+							]}
+							minLength={[
+								EXERCISE_VALID.NAME.minLength,
+								t('Min length is |len|', { len: EXERCISE_VALID.NAME.minLength }),
+							]}
+							label={t('Nomination')}
+						/>
+
+						{!exercise && (
+							<View style={styles.exerciseTypeWrapper}>
+								<Text style={styles.asLabel}>{t('Exercise type')}</Text>
+								<SelectInput items={EXERCISE_TYPES} value={type} onChange={handleSetType} placeholder={{}} />
+								<Text style={styles.exerciseTypeDescription}>
+									{type === ExerciseTypes.Default && t('Default exercise type description')}
+									{type === ExerciseTypes.WithAdditionalWeight && t('WithAdditionalWeight exercise type description')}
+									{type === ExerciseTypes.WithNegativeWeight && t('WithNegativeWeight exercise type description')}
+								</Text>
+							</View>
+						)}
+
+						<StringInputWithValidation
+							defaultValue={description}
+							onChange={handleSetDescription}
+							maxLength={[
+								EXERCISE_VALID.DESCRIPTION.maxLength,
+								t('Max length is |len|', { len: EXERCISE_VALID.DESCRIPTION.maxLength }),
+							]}
+							multiline={true}
+							label={t('Description')}
+						/>
+					</View>
+				</View>
+			</ScrollView>
 
 			<View style={styles.buttonContainer}>
 				<Button solidType="gray" title={t('Cancel')} onPress={handleGoBack} />
@@ -86,6 +133,20 @@ const styles = StyleSheet.create({
 	},
 	h1: {
 		marginVertical: 15,
+	},
+	exerciseTypeDescription: {
+		marginTop: 10,
+		fontSize: FontSizes.Hint,
+	},
+	exerciseTypeWrapper: {
+		marginBottom: 20,
+		paddingHorizontal: 10,
+	},
+	asLabel: {
+		marginBottom: 2,
+		fontSize: FontSizes.Small,
+		fontFamily: Fonts.Kelson,
+		color: Colors.Primary,
 	},
 });
 
